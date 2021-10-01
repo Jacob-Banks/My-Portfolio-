540f2048da03adde47e2dd2c44aae6367c98058a
Jacob Banks <kobybanks@gmail.com>
2021-10-01 18:13:00 -0400

    unstyled  footer added

--- my-portfolio-/src/App.js
+++ my-portfolio-/src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import Footer from "./components/Footer";
 import PortfolioContainer from "./components/PortfolioContainer";
-
-const App = () => <PortfolioContainer />;
+import "./index.css";
+const App = () => {
+  return (
+    <div>
+      <PortfolioContainer></PortfolioContainer>
+      <Footer></Footer>
+    </div>
+  );
+};
 
 export default App;
---
+++ my-portfolio-/src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import phone from "../assets/images/phone.png";
+
+import whatsApp_Logo_7 from "../assets/images/WhatsApp_Logo_7.png";
+
+import gmail from "../assets/images/gmail.png";
+
+import link from "../assets/images/LI-In-Bug.png";
+
+import gitahub from "../assets/images/GitHub-Mark-120px-plus.png";
+function Footer() {
+  return (
+    <footer id="contact">
+      <img src={phone} alt="phone" />
+      <img src={whatsApp_Logo_7} alt="whatsapp" />
+      <a href="mailto:kobybanks@gmail.com">
+        <img src={gmail} alt="gmail logo" />
+      </a>
+      <a href="https://linkedin.com" rel="noreferrer" target="_blank">
+        <img src={link} alt="linkedin logo" />
+      </a>
+      <a href="https://github.com/Jacob-Banks" rel="noreferrer" target="_blank">
+        <img src={gitahub} alt="github logo" />
+      </a>
+    </footer>
+  );
+}
+export default Footer;
